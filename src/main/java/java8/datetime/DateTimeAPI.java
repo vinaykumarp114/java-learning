94f790eeba8c9dd6af2336adb588972897a53a74
chandanashree9 <chandanashree9@gmail.com>
2018-07-24 22:18:23 -0400

    Added few java8 example

--- src/main/java/java8/datetime/DateTimeAPI.java
+++ src/main/java/java8/datetime/DateTimeAPI.java
@@ -13,7 +13,7 @@ public class DateTimeAPI {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
--- src/main/java/java8/string_io/StringAPI.java
+++ src/main/java/java8/string_io/StringAPI.java
@@ -3,6 +3,9 @@
  */
 package java8.string_io;
 
+import java.util.StringJoiner;
+import java.util.stream.IntStream;
+
 /**
  * @author vinay
  *
@@ -13,8 +16,22 @@ public class StringAPI {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		StringAPI stringapi = new StringAPI();
+		
+		stringapi.testString();
+		stringapi.testStringJoiner();
 	}
 
+	private void testString() {
+		String s = "Hello World";
+		IntStream stream = s.chars();
+		stream.mapToObj(c -> (char) c).map(Character::toUpperCase).forEach(System.out::print);
+		System.out.println();
+	}
+
+	private void testStringJoiner() {
+		StringJoiner sj = new StringJoiner(",","{","}");
+		sj.add("One").add("two");
+		System.out.println("String Joiner::"+sj.toString());
+	}
 }
